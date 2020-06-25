916087095be96397d4983f9ae9c0240bb1d4a4df
YumProjects <pilotben@comcast.net>
2020-06-24 23:06:12 -0700

    removed log on getData()

--- script.js
+++ script.js
@@ -93,7 +93,6 @@ function getData(){
     req.onreadystatechange = function(ev){
         if(req.readyState != 4) return;
         data = JSON.parse(req.responseText);
-        console.log(req.responseText);
         window.setTimeout(getData, 5000);
     }
 }
